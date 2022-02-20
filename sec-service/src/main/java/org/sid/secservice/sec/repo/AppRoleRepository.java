705238406235bc8d1b0f0e534eb5bcd40b2946e2
ESSOUFI Othmane <65432235+E-OTHMANE@users.noreply.github.com>
2022-02-20 23:43:26 +0100

    Delete sec-service directory

--- sec-service/src/main/java/org/sid/secservice/sec/repo/AppRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sid.secservice.sec.repo;
-
-import org.sid.secservice.sec.entities.AppRole;
-import org.sid.secservice.sec.entities.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
-    AppRole findByRoleName(String roleName);
-}
